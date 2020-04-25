93d609224c113762d8a24dc50f7f786a2f018b77
Fernando Costa <fernando@nightweb.com.br>
2020-04-25 16:42:02 -0300

    Atualização dos exemplos

--- _examples/radios_status.php
+++ _examples/radios_status.php
@@ -4,7 +4,7 @@
  * Programmer by OnBobba (www.onbobba.com.br)
  */
 
-require_once('_class/OnBobba.php');
+require_once('../_class/OnBobba.php');
 
 $OnBobba = new OnBobba;
 $OnBobba->param('token', 'TOKEN'); // Token Access
